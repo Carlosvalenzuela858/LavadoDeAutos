49ab3238e2544d963eaddc3583842dc7734f8314
Carlosvalenzuela858 <Carloavalenzuela@gmail.com>
2025-10-31 09:41:21 -0300

    Creacion del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,28 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'tabs',
+    loadComponent: () => import('./pages/tabs/tabs.page').then( m => m.TabsPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'reservas',
+    loadComponent: () => import('./pages/reservas/reservas.page').then( m => m.ReservasPage)
+  },
+  {
+    path: 'perfil',
+    loadComponent: () => import('./pages/perfil/perfil.page').then( m => m.PerfilPage)
+  },
 ];
